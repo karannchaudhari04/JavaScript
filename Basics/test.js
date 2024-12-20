447e5cb927edede27a1862ac014f63785c958acb
Karan Chaudhari <149130038+karannchaudhari04@users.noreply.github.com>
2024-12-20 14:26:39 +0000

    initial commit

---
+++ Basics/test.js
@@ -0,0 +1,13 @@
+console.log("Karan")
+const accID = 101;
+let accMail = "abc@gmail.com"
+var accPass = "123456789"
+accCity = "Pune"
+let accState;
+
+console.log(accID);
+console.log(accMail);
+console.log(accPass);
+console.log(accCity);
+
+console.table([accID, accMail, accPass, accCity, accState])
